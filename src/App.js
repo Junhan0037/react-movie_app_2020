84a78e28a1bc95917ab89620022350149ca65e10
Junhan0037 <wnsgks0037@naver.com>
2020-05-27 18:37:14 +0900

    edit file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+function Potato() {
+    return <h1>I love potato.</h1>;
+}
+
 function App() {
-  return <div>Hello!!!</div>;
+  return (
+      <div>
+          <h1>Hello</h1>
+          <Potato />
+      </div>
+  );
 }
 
 export default App;
