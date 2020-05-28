749750387c6b27c789d3c6376c72178ff5e2ae5c
Junhan0037 <wnsgks0037@naver.com>
2020-05-28 20:02:16 +0900

    add state, class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-function Potato() {
-    return <h1>I love potato.</h1>;
-}
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
 
-function App() {
-  return (
-      <div>
-          <h1>Hello</h1>
-          <Potato />
-      </div>
-  );
+    add = () => {
+        this.setState(current => ({
+            count: current.count + 1
+        }));
+    };
+
+    minus = () => {
+        this.setState(current => ({
+            count: current.count - 1
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
 export default App;
